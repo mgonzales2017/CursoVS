2a1a29979b92d99f131be9e94b083372f0eb7400
mgonzales2017 <mgonzalesaguirre@gmail.com>
2017-10-06 21:33:14 -0500

    Clase 06/10/2017

---
+++ DemoTesting/DemoTesting/DemoTesting.Tests/Controllers/AlgebraController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoTesting.Controllers
+{
+    public class AlgebraController : Controller
+    {
+        public ActionResult Sumar(string a, string b)
+        {
+            try
+            {
+                var digitoA = Convert.ToInt32(a);
+                var digitoB = Convert.ToInt32(b);
+                var resultado = digitoA + digitoB;
+                return new JsonResult() { Data = resultado };
+            }
+            catch (NullReferenceException nullex)
+            {
+                throw nullex;
+            }
+            catch (FormatException)
+            {
+                var fm = new FormatException("Se intento sumar caracteres, solo se puede sumar numeros");
+                return new JsonResult() { Data = fm };
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult() { Data = ex };
+            }
+        }
+    }
+}
---
+++ DemoTesting/DemoTesting/DemoTesting.Tests/Controllers/CalculadorControllerTest.cs
@@ -0,0 +1,66 @@
+﻿using DemoTesting.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DemoTesting.Tests.Controllers
+{
+    [TestClass]
+    public class CalculadorControllerTest
+    {
+        [TestMethod]
+        public void Sumar()
+        {
+            // Arrange
+            AlgebraController ac = new AlgebraController();
+            int primerValor = new Random().Next(1, 100);
+            int segundoValor = new Random().Next(1, 200);
+            int resultado = primerValor + segundoValor;
+            // Act
+            var jsonResult = ac.Sumar(primerValor.ToString(), segundoValor.ToString()) as JsonResult;
+
+            // Assert
+            Assert.AreEqual(resultado, jsonResult.Data);
+        }
+
+        [TestMethod]
+        public void Sumar_ConCaracteres()
+        {
+            // Arrange
+            AlgebraController ac = new AlgebraController();
+            var primerValor = "x";
+            var segundoValor = "y";
+            var type = typeof(FormatException);
+            // Act
+            var jsonResult = ac.Sumar(primerValor.ToString(), segundoValor.ToString()) as JsonResult;
+            var jtype = jsonResult.Data.GetType();
+            // Assert
+            Assert.AreEqual(type, jtype);
+        }
+
+        [TestMethod]
+        public void Sumar_ConCaracteresTestMessage()
+        {
+            // Arrange
+            AlgebraController ac = new AlgebraController();
+            var primerValor = "x";
+            var segundoValor = "y";
+            var resultMessage = "Se intento sumar caracteres, solo se puede sumar numeros";
+            // Act
+            var jsonResult = ac.Sumar(primerValor.ToString(), segundoValor.ToString()) as JsonResult;
+            var jsonError = jsonResult.Data as FormatException;
+            // Assert
+            Assert.AreEqual(resultMessage, jsonError.Message);
+
+        }
+
+        public void Sumar_WithLetters()
+        {
+
+        }
+    }
+}
